e20f0592cb14441b53d86c2c5637a3b66887c2ef
Casey <caseysakamoto@cuanschutz.edu>
2020-04-20 11:46:26 -0600

    wrk thes

--- 2019-2020 Brandie/CF Microbiome/Methods/20200203correlations.R
+++ 2019-2020 Brandie/CF Microbiome/Methods/20200203correlations.R
@@ -73,12 +73,81 @@ shm = outpatient2020_01_25 %>% select(visit,sid, ShannonH_Median, ShannonH_Mean)
 # similar enough
 fivenum(shm$ShannonH_Median);fivenum(shm$ShannonH_Mean)
 
-###### corrs for inflammatory markers ####
+###### corrs for inflammatory markers added 4/2020####
+# did this on desktop
 outpatient_2vis <- read.csv("C:/Users/Casey/Desktop/WorkData/Microbiome Brandie 2019-2020/outpatient_2vis.csv")
 sublist = c("BAM014", "BSN024", "ESP008", "MEB022", "NDR020", "SKH012", 
             "VAC001", "VCR009", "A_MO25", "JAR018", "BAR019", "NAO032", "EIL010")
 outpatient_2vis = outpatient_2vis %>% select(lq_all, sid, visit, elas, IL1B, IL8, HMGB_1,
                                              Cell_Counts_x_104,X_Neutrophils___) %>%
   filter(!(sid %in% sublist))%>% unique.data.frame()
-patient_load = outpatient_2vis %>% spread(visit, lq_all) %>%
+
+# load difference visit 2 - 1
+patient_load = outpatient_2vis %>% select(visit, lq_all, sid) %>%spread(visit, lq_all) %>%
   mutate(load_dif = `2`-`1`) %>% select(-`1`,-`2`)
+
+# inflamm difference
+patient_elas  = outpatient_2vis %>% select(visit, elas, sid) %>%spread(visit, elas) %>%
+  mutate(elas_dif = `2`-`1`) %>% select(-`1`,-`2`)
+
+patient_IL1B = outpatient_2vis %>% select(visit, IL1B, sid) %>%spread(visit, IL1B) %>%
+  mutate(IL1B_dif = `2`-`1`) %>% select(-`1`,-`2`)
+
+patient_IL8 = outpatient_2vis %>% select(visit, IL8, sid) %>%spread(visit, IL8) %>%
+  mutate(IL8_dif = `2`-`1`) %>% select(-`1`,-`2`)
+
+patient_HMGB_1 = outpatient_2vis %>% select(visit, HMGB_1, sid) %>%spread(visit,HMGB_1) %>%
+  mutate(HMGB_1_dif = `2`-`1`) %>% select(-`1`,-`2`)
+
+patient_cellct = outpatient_2vis %>% select(visit, Cell_Counts_x_104, sid) %>%spread(visit,Cell_Counts_x_104) %>%
+  mutate(cellct_dif = `2`-`1`) %>% select(-`1`,-`2`)
+
+patient_neutrophil = outpatient_2vis %>% select(visit, X_Neutrophils___, sid) %>%spread(visit,X_Neutrophils___) %>%
+  mutate(neutrophils_dif = `2`-`1`) %>% select(-`1`,-`2`)
+
+# df
+# quite a few missing a visit for neutrophils and white cell ct
+# subjects typically missing all inflam data or neutrophils or white cell cct
+patient_inflam = full_join(patient_load, patient_elas, by = "sid")
+patient_inflam = full_join(patient_inflam, patient_IL1B, by = "sid")
+patient_inflam = full_join(patient_inflam, patient_IL8, by = "sid")
+patient_inflam = full_join(patient_inflam, patient_HMGB_1, by = "sid")
+patient_inflam = full_join(patient_inflam, patient_cellct, by = "sid")
+patient_inflam = full_join(patient_inflam, patient_neutrophil, by = "sid")
+
+par(mfrow = c(3,2))
+load_elas_cor = ggplot(data = patient_inflam) + 
+  geom_point(aes(load_dif,elas_dif)) + theme_classic() +
+  labs(title = "Change in Log-Copies/Reaction (V2-V1) vs Change in Elastase",
+       x = "Change in Log-Copies/Reaction", y = "Change in Elastase")
+print(load_elas_cor) # aa007 big outlier
+
+load_IL1B_cor = ggplot(data = patient_inflam) + 
+  geom_point(aes(load_dif,IL1B_dif)) + theme_classic() +
+  labs(title = "Change in Log-Copies/Reaction (V2-V1) vs Change in IL-1B",
+       x = "Change in Log-Copies/Reaction", y = "Change in IL-1B")
+print(load_IL1B_cor)# lme026 big outlier
+
+load_IL8_cor = ggplot(data = patient_inflam) + 
+  geom_point(aes(load_dif,IL8_dif)) + theme_classic() +
+  labs(title = "Change in Log-Copies/Reaction (V2-V1) vs Change in IL-8",
+       x = "Change in Log-Copies/Reaction", y = "Change in IL-8")
+print(load_IL8_cor) # this one is strange
+
+load_HMGB_cor = ggplot(data = patient_inflam) + 
+  geom_point(aes(load_dif,HMGB_1_dif)) + theme_classic() +
+  labs(title = "Change in Log-Copies/Reaction (V2-V1) vs Change in HMGB-1",
+       x = "Change in Log-Copies/Reaction", y = "Change in HMGB-1")
+print(load_HMGB_cor) # JWS CAM outliers
+
+load_cellct_cor = ggplot(data = patient_inflam) + 
+  geom_point(aes(load_dif,cellct_dif)) + theme_classic() +
+  labs(title = "Change in Log-Copies/Reaction (V2-V1) vs Change in White Blood Cell Ct",
+       x = "Change in Log-Copies/Reaction", y = "Change in White Blood Cell ct")
+print(load_cellct_cor) # lots of missing here
+
+load_neutro_cor = ggplot(data = patient_inflam) + 
+  geom_point(aes(load_dif,neutrophils_dif)) + theme_classic() +
+  labs(title = "Change in Log-Copies/Reaction (V2-V1) vs Change in % Neutrophil",
+       x = "Change in Log-Copies/Reaction", y = "Change in % Neutrophil")
+print(load_neutro_cor) # lots of missing data
